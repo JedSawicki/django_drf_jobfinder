199dc7e3b5546ca6d4dcd6399ef6a8b0aa5e2f63
JedSawicki <jedsawicki@gmail.com>
2022-08-03 16:19:05 +0200

    Added offer model

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraper'
+    'scraper.apps.ScraperConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
