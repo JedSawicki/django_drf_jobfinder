1bb2b6500cb36d6244c08fdbb4ce6d9f473f56b0
JedSawicki <jedsawicki@gmail.com>
2022-09-20 18:34:12 +0200

    Added HTML template
    Raw template from web

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import HomeView
 
 urlpatterns = [
     path('scraper/', include('scraper.urls')),
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
