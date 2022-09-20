665228d0ac45d06ed42cd5b58dea303a6ebe0665
JedSawicki <jedsawicki@gmail.com>
2022-09-20 18:48:59 +0200

    Hotfix v.1 static files

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from .views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('scraper/', include('scraper.urls')),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
