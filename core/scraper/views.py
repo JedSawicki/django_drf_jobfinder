ff67a49d82a3fbf48f639b0776d487e2bfbbb654
Jedrzej Sawicki <jedsawicki@gmail.com>
2023-01-26 10:51:53 +0100

    Refactored workers, new product scraper demo

--- core/scraper/views.py
+++ core/scraper/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.renderers import TemplateHTMLRenderer
 from .models import Offer
 from .serializers import OfferSerializer, CreateOfferSerializer
-from .worker import Scraper
+from .worker.worker import Scraper
 
 
 class OfferList(generics.ListCreateAPIView):
